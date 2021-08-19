8d63beb630c65f8f6638e561a31510b2554d439c
NISS <nishansubedi15@gmail.com>
2021-08-19 19:20:21 +0545

    wrong answer

--- task_8.php
+++ task_8.php
@@ -3,16 +3,24 @@
 <body>
 <form>  
 Enter First Number:  
-<input type="number" name="number1" /><br><br>  
+<input type="number" name="x" /><br><br>  
 Enter Second Number:  
-<input type="number" name="number2" /><br><br>  
+<input type="number" name="y" /><br><br>  
 <input  type="submit" name="submit" value="Add">  
 
 </form>  
 <?php
+echo array_sum(range(1,$y));
+function sum($x,$y){
+if($x%sum) == 0)
+echo (array_sum);
+}
+else 
+ echo sum"null";
+//echo array_sum(range(1,$y));
 
-function ($number1,$number2)
-echo array_sum(range(1,$number2));
+}
+
 ?>
 
 </body>
